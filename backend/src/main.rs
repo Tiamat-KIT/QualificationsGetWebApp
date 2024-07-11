2a447917f4dbc4255fc7678f5dc596e35a453dc5
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2024-07-11 16:21:56 +0000

    結構むずいのでいったんやめる

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,17 +4,14 @@ use std::{
 };
 use axum::{routing::{get, post}, Json, Router};
 
-use convex::{ConvexClient, FunctionResult};
+use convex::{ConvexClient, ConvexError, FunctionResult, Value};
 
 #[tokio::main]
 async fn main() {
-    async fn add_handler() {
-        let deployment_url = get_deployment_url();
-        let mut client = ConvexClient::new(&deployment_url).await.unwrap();
-        let _result = client.mutation("exam:testAdd", BTreeMap::new()).await.unwrap();
-    }
-    let app = Router::new().route("/add",get(add_handler));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/exam/list", get(get_all_exam_handler));
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
     axum::serve(listener,app).await.unwrap();
 }
 
@@ -26,3 +23,42 @@ fn get_deployment_url() -> String{
 
     return deployment_url.clone();
 }
+
+async fn get_client() -> ConvexClient{
+    let deployment_url = get_deployment_url();
+    let client = ConvexClient::new(&deployment_url).await.unwrap();
+    return client
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+/* async fn add_exam_handler() {
+    let mut client = get_client().await;
+    let _result = client.mutation("exam:testAdd", BTreeMap::new()).await.unwrap();
+} */
+
+async fn get_all_exam_handler() -> String{
+    let mut client = get_client().await;
+    let result = client.query("exam:getAllExams", BTreeMap::new())
+        .await.unwrap();
+    let string_value = match result {
+        FunctionResult::Value(Value::Object(object)) => {
+            let mut elements = String::new();
+            for(key,value) in object {
+                elements.push_str(&format!("{}: {:?}", key, value));
+            }
+            elements
+        },
+        FunctionResult::ErrorMessage(msg) => {
+            return msg
+        },
+        FunctionResult::ConvexError(ConvexError { message: err, data }) => {
+            return err
+        },
+        _ => todo!(),
+    };
+    return string_value
+}
+
